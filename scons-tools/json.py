a318e88636eaf9c23af9e2671cc9dd23f89661dd
Eldritch Cheese <eldritch.cheese@gmail.com>
2017-09-19 13:12:07 -0700

    Added nlohmann/json package

---
+++ scons-tools/json.py
@@ -0,0 +1,26 @@
+
+
+def exists(env):
+    return True
+
+def generate(env):
+    inc_dir = env['dep_dir'].Dir('json-master/src')
+
+    usage = {'CPPSYSTEMPATH': [inc_dir],
+             }
+
+    if inc_dir.exists():
+        return usage
+
+    import urllib2
+    import StringIO
+    import zipfile
+
+    response = urllib2.urlopen('https://github.com/nlohmann/json/archive/master.zip')
+    contents = StringIO.StringIO(response.read())
+    zipped = zipfile.ZipFile(contents)
+    members = [filename for filename in zipped.namelist()
+               if 'json.hpp' in filename or 'LICENSE' in filename]
+    zipped.extractall(env['dep_dir'].abspath, members)
+
+    return usage
