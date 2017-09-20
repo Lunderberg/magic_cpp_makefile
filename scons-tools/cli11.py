35b2ace13cd3da965fb591aceba018989bbbdfc4
Eldritch Cheese <eldritch.cheese@gmail.com>
2017-09-19 21:26:16 -0700

    Added CLI11

---
+++ scons-tools/cli11.py
@@ -0,0 +1,27 @@
+
+
+def exists(env):
+    return True
+
+def generate(env):
+    inc_dir = env['dep_dir'].Dir('CLI11')
+
+    usage = {'CPPSYSTEMPATH': [inc_dir],
+             }
+
+    if inc_dir.exists():
+        return usage
+
+    import os
+    import urllib2
+
+    response = urllib2.urlopen('https://github.com/CLIUtils/CLI11/releases/download/'
+                               'v1.1.0/CLI11.hpp')
+    contents = response.read()
+
+    os.makedirs(str(inc_dir))
+    filename = os.path.join(str(inc_dir), 'CLI11.hpp')
+    with open(filename, 'w') as f:
+        f.write(contents)
+
+    return usage
